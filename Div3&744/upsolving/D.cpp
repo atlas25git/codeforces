78345e708e8852353a0e13d89f190b241f8ba806
atlas25git <atlas.sarthak@gmail.com>
2021-10-11 14:30:55 +0530

    744&745

---
+++ Div3&744/upsolving/D.cpp
@@ -0,0 +1,120 @@
+/***************************************
+*****Solution authored by Atlas25.******
+*****************************************/
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define P pair<int,int>
+#define pb push_back
+const int mod = 1e9 + 7;
+const int N = 2e5 + 1;
+const int MOD = 1e9 + 7;
+int fact[N], inv[N], invfact[N];
+void factInverse() {
+fact[0] = inv[1] = fact[1] = invfact[0] = invfact[1] = 1;
+for (long long i = 2; i < N; i++) {
+fact[i] = (fact[i - 1] * i) % MOD;
+inv[i] = MOD - (inv[MOD % i] * (MOD / i) % MOD);
+invfact[i] = (inv[i] * invfact[i - 1]) % MOD;
+}
+}
+const std::string WHITESPACE = " \n\r\t\f\v";
+std::string ltrim(const std::string &s)
+{
+size_t start = s.find_first_not_of(WHITESPACE);
+return (start == std::string::npos) ? "" : s.substr(start);
+}
+std::string rtrim(const std::string &s)
+{
+size_t end = s.find_last_not_of(WHITESPACE);
+return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+std::string trim(const std::string &s) {
+return rtrim(ltrim(s));
+}
+int add(int a, int b) {
+if ((a += b) >= MOD)
+    a -= MOD;
+else if (a < 0)
+    a += MOD;
+return a;
+}
+int mul(int x, int y) {
+    return (1LL * x * y) % MOD;
+}
+int nCr(int n, int r) {
+    if (r > n)
+        return 0;
+    return mul(mul(fact[n], invfact[r]), invfact[n - r]);
+}
+vector<string> tokenize(string s, const char* del)
+{
+    vector<string> res;
+    auto x = s.c_str();
+    char y[s.size()+1];
+    strcpy(y,x);
+    auto tok = strtok(y,del);
+    while(tok)
+    {
+    res.push_back(string(tok));
+    tok = strtok(0,del);
+    }
+    return res;
+}
+void solve(){
+    int i,j,k,n,m,an1s=0,cnt=0,sum=0;
+        cin>>n;
+        auto cmp = [](pair<int,int> const &x, pair<int,int> const &y){
+            return x>y;
+        };
+
+        set<P,decltype(cmp)> a(cmp);
+        // set<P> a;
+        vector<P> ans;
+
+        for(int i=0;i<n;i++)
+        {
+            int ai;
+            cin>>ai;
+            if(ai>0)
+                a.emplace(ai,i+1);
+        }
+
+        while(a.size()>1){
+            auto p1 = *a.begin();
+            a.erase(a.begin());
+            auto p2 = *a.begin();
+            a.erase(a.begin());
+
+            ans.emplace_back(p1.second,p2.second);
+            if(p1.first>1)a.emplace(p1.first-1,p1.second);
+            if(p2.first>1)a.emplace(p2.first-1,p2.second);
+        }
+
+        cout<<ans.size()<<"\n";
+
+        for(auto &p : ans)
+            cout<<p.first<<" "<<p.second<<"\n";
+
+
+}
+void init() {
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+int32_t main(){
+    init();
+        {
+            int t;
+            cin>>t;
+            while(t--)
+            solve();
+        }
+    }
